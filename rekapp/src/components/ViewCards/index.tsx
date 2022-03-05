a74beb1378bbd31120ae680686e1afabe9a8a48e
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2022-03-05 11:40:53 -0400

    buttons interface added and remove button working at ViewCards component

--- rekapp/src/components/ViewCards/index.tsx
+++ rekapp/src/components/ViewCards/index.tsx
@@ -12,28 +12,45 @@ import CardInfo from '../CardInfo';
 function ViewCards() {
 
     const [cards, setCards] = useState<Card[] | never[]>([]);
+    const [update, setUpdate] = useState(false);
     const [loading, setLoading] = useState<boolean>(true);
 
     const deckId = useSelector((state : RootState) => state.viewCards);
 
-    const getCardsFromDeckId = async (DeckId: string) => {
+    const getCardsFromDeckId = async (deckId: string) => {
         const deckInfo : Deck =  (await axios.get(`/deck/${deckId}`)).data;
         setCards(deckInfo.cards);
         setLoading(false);
     }
 
+    const deleteCard = async (cardId: string | undefined) => {
+        if(cardId){
+            const response = await axios.delete(`/card/${deckId}/${cardId}`);
+            if(response.status === 204){
+                setUpdate(prev => !prev);
+            }
+        }
+    };
+
 
     useEffect(() => {
         getCardsFromDeckId(deckId);
-    }, []);
+    }, [update]);
 
     return (
         !loading ? (
-            <div className={styles.container}>
+            <div className={styles.view_cards_container}>
                 {cards.map(card => {
                     return (<div className={styles.rows}>
+
                         <CardInfo small={true} cardInfo={{img: card.frontImg, audio: card.frontAudio, text: card.front}} />
                         <CardInfo small={true} cardInfo={{img: card.backImg, audio: card.backAudio, text: card.back}} />
+
+                        <div>
+                            <button className={styles.view_cards_button} onClick={() => deleteCard(card._id)}><i className="fas fa-trash"></i></button>
+                            <button className={styles.view_cards_button}><i className="fas fa-edit"></i></button>
+                        </div>
+
                         </div>)
                 })}
             </div>
