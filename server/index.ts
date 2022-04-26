d0e8a488a6dd742a4a796c57487f3fc6d9430d6f
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2022-04-25 20:53:05 -0400

    Procfile added

--- server/index.ts
+++ server/index.ts
@@ -1,47 +1,5 @@
-//import app from './app';
-
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
-dotenv.config();
-
-import './database';
-
-//ROUTES
-import authRoutes from './routes/auth.routes';
-import deckRoutes from './routes/deck.routes';
-
-const app = express();
-
-app.use(express.json({limit: "30mb"}));
-app.use(express.urlencoded({extended: true, limit: "30mb"}));
-
-app.use(cors());
-
-// SERVE REACT APP
-
-app.use(express.static(path.resolve(__dirname, '../rekapp/build')));
-
-app.get("/api", (req, res) => {
-    res.json({msg: "Hello world"});
-})
-
-/*
-
-// to redirect to our react app if a url doesn't exists
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../rekapp/build', 'index.html'));
-});
-
-*/
-
-
-
-app.use("/auth", authRoutes);
-app.use("/api", deckRoutes);
+import app from './app';
 
 const PORT : number = parseInt((process.env.PORT as string)) | 3001;
 
-app.listen(PORT, "0.0.0.0", () => console.log(`Server listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
